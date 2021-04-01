8349e679e861555388071f70d73f1eb88e87a86a
Evgeniya Kotova <zheniyasl@gmail.com>
2021-04-01 11:59:54 +0200

    get data from wetherbit

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,6 +1,8 @@
 /* Global Variables */
 // openweather url address
 const baseURL = "http://api.geonames.org/searchJSON?q=";
+const baseWeatherbitURL = "https://api.weatherbit.io/v2.0/";
+
 let apiKeys = {};
 // Create a new date instance dynamically with JS
 let d = new Date();
@@ -38,7 +40,7 @@ function performAction(e) {
     alert("Please enter city's name");
   } else {
     getApiKeys()
-      .then((apiKeys) => getGeonamesData(city))
+      .then(() => getGeonamesData(city))
       .then((data) => {
         // let userInput = document.getElementById("feelings").value;
         let resultData = {};
@@ -51,6 +53,7 @@ function performAction(e) {
         let daysLeft = (tripStart - today) / (1000 * 60 * 60 * 24) + 1;
         console.log(resultData);
         console.log(daysLeft);
+        return resultData;
 
         //let leftDays =
         // postData("http://localhost:3000/add", {
@@ -60,7 +63,8 @@ function performAction(e) {
         //   userInput: userInput,
         //   weather: weatherDescription,
         // });
-      });
+      })
+      .then((resultData) => getWeatherAndPicture(resultData));
     //.then(updateUI);
   }
 }
@@ -87,8 +91,40 @@ const getGeonamesData = async (city) => {
     console.log(data);
     return data;
   } catch (error) {
-    console.log("error", eror);
+    console.log("error", error);
   }
+};
+
+const getWeatherAndPicture = (inputData) => {
+  getWeatherbitData(inputData).then((data) => {
+    console.log("data", data);
+  });
+};
+const getWeatherbitData = async (inputData) => {
+  let data = {};
+  //get current weather
+  const res = await fetch(
+    `${baseWeatherbitURL}current?lat=${inputData.lat}&lon=${inputData.lng}&key=${apiKeys.API_WEATHERBIT}`
+  );
+  try {
+    const currentWeather = await res.json();
+    console.log(currentWeather);
+    data["currentWeather"] = currentWeather;
+  } catch (error) {
+    console.log("error", error);
+  }
+  // get weather forecast
+  const res_forecast = await fetch(
+    `${baseWeatherbitURL}forecast/daily?lat=${inputData.lat}&lon=${inputData.lng}&key=${apiKeys.API_WEATHERBIT}`
+  );
+  try {
+    const dailyWeather = await res_forecast.json();
+    console.log(dailyWeather);
+    data["dailyWeather"] = dailyWeather;
+  } catch (error) {
+    console.log("error", error);
+  }
+  return data;
 };
 
 const updateUI = async () => {
